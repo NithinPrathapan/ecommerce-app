9158c8e196552997f942fed96f5c55c4df05e483
Nithin Prathapan <nithinprathapan369@gmail.com>
2024-03-01 21:22:57 +0530

    mongo connection established

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 const allowedOrigins =
   process.env.NODE_ENV === "production"
@@ -24,4 +26,12 @@ app.use((req, res, next) => {
 
 const port = 5000;
 
-app.listen(port, () => console.log("server connected to port", port));
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => console.log("server connected to port", port));
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
